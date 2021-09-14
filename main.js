b3cf84f22a5c9b12d79b4ee1f51912eb85df00d7
euanrodger <euanrod67@gmail.com>
2021-09-14 21:01:23 +0100

    Bare minimum
    Added the bare minimum to get the bot online and connected to a server.

---
+++ main.js
@@ -0,0 +1,13 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with the client token
+client.login(token);
