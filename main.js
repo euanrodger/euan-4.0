c728be3ae9f9c5a325d71bf41f98c3344edfde9b
euanrodger <euanrod67@gmail.com>
2021-09-14 21:15:32 +0100

    Added basic commands

--- main.js
+++ main.js
@@ -9,5 +9,22 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+// command handling
+client.on('interactionCreate', async interaction => {
+  // Check if interaction is command
+	if (!interaction.isCommand()) return;
+
+  // Check which command it is
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nServer birthday: ${interaction.guild.createdAt}\nVerification level: ${interaction.guild.verificationLevel}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 // Login to Discord with the client token
 client.login(token);
