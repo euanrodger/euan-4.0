e9be5c28ab1785e144ca4f2c41e2b85399f0070b
euanrodger <euanrod67@gmail.com>
2021-09-14 22:48:37 +0100

    More command improvements
    some grammar stuff, and user command now has an option to select a user other than yourself

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with the bot\'s ping'),
+		.setDescription('Replies with the bot\'s ping.'),
 	async execute(interaction) {
-		await interaction.reply({ content: `pong! ${interaction.client.ws.ping}ms` });
+		await interaction.reply({ content: `Pong! ${interaction.client.ws.ping}ms` });
 	},
 };
--- commands/user.js
+++ commands/user.js
@@ -3,8 +3,11 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Replies with user info'),
+		.setDescription('Replies with user info of the selected user, or user info for yourself.')
+		.addUserOption(option => option.setName('target').setDescription('The user\'s info to show')),
 	async execute(interaction) {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+		const user = interaction.options.getUser('target');
+		if (user) return interaction.reply(`${user.username}'s tag: ${user.tag}\n${user.username}'s id: ${user.id}`);
+		return interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	},
 };
