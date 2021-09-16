25f67662cf98fb4fd6c9c3c9970b7ebbdc28b7f1
euanrodger <euanrod67@gmail.com>
2021-09-16 23:44:40 +0100

    Don't add bot messages to markov chains

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,7 +4,9 @@ module.exports = {
     const timestamp = `[${message.createdAt.getHours()}:${message.createdAt.getMinutes()}:${message.createdAt.getSeconds()}]`
 		console.log(`${timestamp} In ${message.guild.name}, ${message.author.tag} said "${message.content}" in #${message.channel.name}.`);
 
-    message.client.markov.addStates(message.content);
-		console.log(message.client.markov.getStates());
+    if (!message.author.bot) {
+      message.client.markov.addStates(message.content);
+      console.log('Message added to markov states!');
+  }
   },
 };
