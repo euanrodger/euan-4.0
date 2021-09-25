878217977b32399bdf556776c25483eafefbf46d
euanrodger <euanrod67@gmail.com>
2021-09-25 13:27:02 +0100

    Random message sending
    Euan 4.0 will now occasionally send messages without needing prompted to do so.
    
    In future it'll be nice to have a command to set the chance messages come out.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,6 +10,16 @@ module.exports = {
       message.client.markov.addStates(message.content);
       message.client.hasStates = true;
       console.log('Message added to markov states!');
-  }
+    }
+    // Say something randomly
+    if (!message.author.bot && message.client.isTrained == true) {
+      const messageChance = 0.05;
+      const hit = Math.random();
+      if (hit <= messageChance) {
+        console.log('That\'s a hit, sending random message')
+        const textToSend = message.client.markov.generateRandom(1999);
+        message.channel.send(textToSend);
+      }
+    }
   },
 };
